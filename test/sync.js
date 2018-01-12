320c9ea4d74e0351d95b2049d5f9881777f4638d
Kevin Ross <kevin.ross@alienfast.com>
2018-01-12 09:48:13 -0600

    reorganize test structure under the context of method name (no other changes)

--- test/sync.js
+++ test/sync.js
@@ -1,109 +1,115 @@
 import { object, string } from '../src';
 
 describe('Sync', () => {
-  it('should validate', () => {
-    const schema = object().shape({
-      name: string().required(),
-      email: string().email(),
-    });
-    const data = {
-      name: 'John Doe',
-      email: 'john@example.com',
-    };
-
-    let result;
-    let error;
-    schema
-      .validate(data, {
-        sync: true,
-      })
-      .then(res => {
-        result = res;
-      })
-      .catch(err => {
-        error = err;
+  describe('validate', () => {
+    it('should be valid', () => {
+      const schema = object().shape({
+        name: string().required(),
+        email: string().email(),
       });
+      const data = {
+        name: 'John Doe',
+        email: 'john@example.com',
+      };
 
-    expect(result).to.deep.equal(data);
-    expect(error).to.be.undefined; // eslint-disable-line no-unused-expressions
-  });
+      let result;
+      let error;
+      schema
+        .validate(data, {
+          sync: true,
+        })
+        .then(res => {
+          result = res;
+        })
+        .catch(err => {
+          error = err;
+        });
 
-  it('should throw', () => {
-    const schema = object().shape({
-      email: string().email(),
+      expect(result).to.deep.equal(data);
+      expect(error).to.be.undefined; // eslint-disable-line no-unused-expressions
     });
-    const data = {
-      email: 'johnexample.com',
-    };
 
-    let result;
-    let error;
-    schema
-      .validate(data, {
-        sync: true,
-      })
-      .then(res => {
-        result = res;
-      })
-      .catch(err => {
-        error = err;
+    it('should throw', () => {
+      const schema = object().shape({
+        email: string().email(),
       });
+      const data = {
+        email: 'johnexample.com',
+      };
 
-    expect(result).to.be.undefined; // eslint-disable-line no-unused-expressions
-    expect(error.name).to.equal('ValidationError');
-    expect(error.path).to.equal('email');
+      let result;
+      let error;
+      schema
+        .validate(data, {
+          sync: true,
+        })
+        .then(res => {
+          result = res;
+        })
+        .catch(err => {
+          error = err;
+        });
+
+      expect(result).to.be.undefined; // eslint-disable-line no-unused-expressions
+      expect(error.name).to.equal('ValidationError');
+      expect(error.path).to.equal('email');
+    });
   });
 
-  it('should validateSync', () => {
-    const schema = object().shape({
-      name: string().required(),
-      email: string().email(),
-    });
-    const data = {
-      name: 'John Doe',
-      email: 'john@example.com',
-    };
+  describe('validateSync', () => {
+    it('should be valid', () => {
+      const schema = object().shape({
+        name: string().required(),
+        email: string().email(),
+      });
+      const data = {
+        name: 'John Doe',
+        email: 'john@example.com',
+      };
 
-    const result = schema.validateSync(data);
-    expect(result).to.deep.equal(data);
+      const result = schema.validateSync(data);
+      expect(result).to.deep.equal(data);
+    });
+
+    it('should throw', () => {
+      const schema = object().shape({
+        email: string().email(),
+      });
+      const data = {
+        email: 'johnexample.com',
+      };
+
+      (() => {
+        schema.validateSync(data);
+      }).should.throw();
+    });
   });
 
-  it('should throw on validateSync', () => {
-    const schema = object().shape({
-      email: string().email(),
+  describe('isValidSync', () => {
+    it('should return true', () => {
+      const schema = object().shape({
+        name: string().required(),
+        email: string().email(),
+      });
+      const data = {
+        name: 'John Doe',
+        email: 'john@example.com',
+      };
+
+      const result = schema.isValidSync(data);
+      expect(result).to.equal(true);
     });
-    const data = {
-      email: 'johnexample.com',
-    };
 
-    (() => {
-      schema.validateSync(data);
-    }).should.throw();
-  });
+    it('should return false', () => {
+      const schema = object().shape({
+        email: string().email(),
+      });
+      const data = {
+        email: 'johnexample.com',
+      };
 
-  it('should return true from isValidSync', () => {
-    const schema = object().shape({
-      name: string().required(),
-      email: string().email(),
+      const result = schema.isValidSync(data);
+      expect(result).to.equal(false);
     });
-    const data = {
-      name: 'John Doe',
-      email: 'john@example.com',
-    };
-
-    const result = schema.isValidSync(data);
-    expect(result).to.equal(true);
-  });
-
-  it('should return false from isValidSync', () => {
-    const schema = object().shape({
-      email: string().email(),
-    });
-    const data = {
-      email: 'johnexample.com',
-    };
-
-    const result = schema.isValidSync(data);
-    expect(result).to.equal(false);
   });
 });
